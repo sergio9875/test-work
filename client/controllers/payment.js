55d8ad76c3ded9c10057bced9f05d43aa41e62eb
sergio9875 <sergio9875@gmail.com>
2022-02-22 00:48:39 +0200

    send payment data to UI

--- client/controllers/payment.js
+++ client/controllers/payment.js
@@ -9,15 +9,13 @@ const underPayments = require('../utils/underPayment')
 const correctEmail = require('../utils/isEmailCorrect')
 const isDuplicatePayment = require('../utils/duplicatedPayment')
 const isAmountTreshold = require('../utils/amountTreshold')
-const createObj = require('../utils/createObj')
-const users = require('../models/paymentModel')
+let payData = require('../request.json')
+
 
 
 module.exports = {
-
     async newPayment (req, res) {
         try{
-
             const { amount, student_id, email  } = req.body
             const amountFee = await amountWithFee(amount)
             const amountRes =  await amountReceived(amountFee)
@@ -33,37 +31,26 @@ module.exports = {
                 underPayment: underPay,
                 qualityCheck: {
                     invalidEmail: await correctEmail(email),
-                    duplicatedPayment: await isDuplicatePayment(student_id, users),
-                    amountTreshold: await isAmountTreshold(users)
+                    duplicatedPayment: await isDuplicatePayment(student_id, payData),
+                    amountTreshold: await isAmountTreshold(payData)
                 }
             }
             if (!newUser.reference || !newUser.amount)
                 return res.sendStatus(400)
 
-
-            let obj = await createObj(newUser, users)
-            // if(!users) {
-            //     fs.writeFile('request.json', JSON.stringify(users), 'utf8',function(err){
-            //         if(err) return console.log(err)});
-            // } else {
-            //
-            //     const fileData = JSON.parse(fs.readFile('request.json'))
-            //     console.log(fileData)
-            //     fileData.push(newUser)
-            //     fs.appendFile('request.json', JSON.stringify(users), 'utf8',function(err){
-            //         if(err) return console.log(err)});
-            // }
-            return res.status(200).send(JSON.stringify(obj))
-
+           if(payData) {
+             payData.booking_with_quality_check.push(newUser)
+             fs.writeFile('request.json', JSON.stringify(payData), 'utf8',function(err){
+             if(err) return console.log(err)});
+ }
+           return res.status(200).send(JSON.stringify(payData))
         } catch (e) {
             console.log(e)
         }
-
     },
-
     async paymentPage  (req, res, ) {
-        console.log('homepage')
         res.sendFile(path.join(__dirname+'../../src/views/booking.html'));
     },
+
 }
 
--- client/utils/duplicatedPayment.js
+++ client/utils/duplicatedPayment.js
@@ -1,7 +1,6 @@
 module.exports = async ( user_id, data ) => {
-
   const isDuplicate =  data.booking_with_quality_check.findIndex(function(item, i){
-        return item.user_id === user_id });
+      return item.user_id === user_id });
   if(isDuplicate === -1 ) return false
   if(isDuplicate) return true
 }
